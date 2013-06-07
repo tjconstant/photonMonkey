cf308fc4ba6be4b0c9ef21768d4e0d22525724a3
Tom Constant <t.j.constant@gmail.com>
2013-06-07 16:11:09 +0100

    Turning disp.plot4 into a function.

--- R/disp.plot4.R
+++ R/disp.plot4.R
@@ -1,18 +1,16 @@
-data(SPPdispersion)
-x<-SPPdispersion$wavelength*1e9 #needed to make scales for x and y similar for interp()
-y<-SPPdispersion$angle
-z<-SPPdispersion$reflection
+disp.plot4<-function(x,y,z,fx=nothing_x,fy=nothing_y,nx=length(unique(x)),ny=length(unique(y)),...){
 
-#disp.plot3(x,y,z,(2*pi/(x*1e-9))*sin(y*pi/180),2*pi*3e8/(x*1e-9))
+  interp<-akima::interp
+  int_spp<-interp(x,y,z,xo=seq(min(x),max(x),,nx),yo=seq(min(y),max(y),,ny))
+  
+  x2<-matrix(int_spp$x,nrow=nx,ncol=ny)
+  y2<-matrix(int_spp$y,nrow=nx,ncol=ny,byrow=T)
+  z2<-int_spp$z
+  
+  image.plot<-fields::image.plot
+  image.plot(fx(x2,y2),fy(x2,y2),z2,xaxs='i',yaxs='i',...)
 
-smooth_value<-400
+}
 
-library(akima)
-int_spp<-interp(x,y,z,xo=seq(min(x),max(x),,smooth_value),yo=seq(min(y),max(y),,smooth_value))
-
-x2<-matrix(int_spp$x,nrow=smooth_value,ncol=smooth_value)
-y2<-matrix(int_spp$y,nrow=smooth_value,ncol=smooth_value,byrow=T)
-z2<-int_spp$z
-
-image.plot((2*pi/(x2*1e-9))*sin(y2*pi/180),2*pi*3e8/(x2*1e-9),z2,xaxs='i',yaxs='i',col=grey(0:100/100))
-#add.diffraction(kgx=2*pi/600e-9,kgv=2*pi/150e-9)
+nothing_x<-function(x,y) return(x)
+nothing_y<-function(x,y) return(y)
