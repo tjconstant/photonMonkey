ab11d51418926357463fedd2417a48e1da042f45
Tom Constant <t.j.constant@gmail.com>
2013-07-02 21:30:37 +0100

    longevity

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   
   cat("\nWelcome to photonMonkey! \nUtility functions for common tasks in electromagnetics.")
   
-  wisdom_index<-sample(1:6,1,replace=T)
+
   wisdom<-c("MATLAB is for engineers and Pandas.",
             "Asking questions in group meetings won't make your dick any bigger.",
             "That thesis isn't going to write itself.",
@@ -12,5 +12,8 @@
             "A tower run under 40 seconds is an achievement. Under 35 seconds is a wasted life.",
             "The cleaner may knife you."
             )
+  
+  wisdom_index<-sample(1:length(wisdom),1,replace=T)
+  
   cat(paste("\nA little wisdom:",wisdom[wisdom_index]))
 }
