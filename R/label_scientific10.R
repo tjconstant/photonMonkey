03251177e3bb44a573af8c708f1eed1613616b6b
tjconstant <tjc@toms-mbp.home>
2013-10-27 00:10:16 +0100

    added example

--- R/label_scientific10.R
+++ R/label_scientific10.R
@@ -1,7 +1,9 @@
 label_scientific10 <- function(x) {
   
+  scientific_format<-scales::scientific_format
+  
   a<-gsub("e", " %*% 10^", scientific_format()(x))
-  b<-gsub("+","",a)
+  b<-gsub("\\+","",a)
   b[x==0]<-"0"
   return(parse(text=b))
 }
